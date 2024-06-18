f86c87de7c6ffe8115967d0288feeca8d86568ca
Satvik <satvik.km@kapturecrm.com>
2024-06-18 18:29:01 +0530

    basic css fix

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -15,43 +15,43 @@ const Sidebar = () => {
   return (
     <aside>
       <div className="description">Widget Library.</div>
-      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'Message')} draggable>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'Message')} draggable>
         <div>
-          <ChatBubbleIcon fontSize="medium" className="icon" /><span style={{ marginLeft: '15%' }}>Send Message</span>
+          <ChatBubbleIcon fontSize="medium" className="icon" /><span>Send Message</span>
         </div>
-        <div className='ml-5'>
+        <div className="sub-txt">
           <p>Send a message.</p>
         </div>
       </div>
       <div className="dndnode" onDragStart={(event) => onDragStart(event, 'Options')} draggable>
         <div>
-          <ListIcon fontSize="medium" className="icon" /><span style={{ marginLeft: '14%' }}>Select Messages</span>
+          <ListIcon fontSize="medium" className="icon" /><span>Select Messages</span>
         </div>
-        <div className='ml-9'>
+        <div className="sub-txt">
           <p>Select a list of options.</p>
         </div>
       </div>
       <div className="dndnode api" onDragStart={(event) => onDragStart(event, 'Api Caller')} draggable>
         <div>
-          <ApiIcon fontSize="medium" className="icon" /><span style={{ marginLeft: '15%' }}>Request an API</span>
+          <ApiIcon fontSize="medium" className="icon" /><span>Request an API</span>
         </div>
-        <div className='ml-7'>
+        <div className="sub-txt">
           <p>Perform an API request.</p>
         </div>
       </div>
       <div className="dndnode form-node" onDragStart={(event) => onDragStart(event, 'Create Form')} draggable>
         <div>
-          <DynamicFormIcon fontSize="medium" className="icon" /><span style={{ marginLeft: '18%' }}>Create Form</span>
+          <DynamicFormIcon fontSize="medium" className="icon" /><span>Create Form</span>
         </div>
-        <div className='ml-5'>
+        <div className="sub-txt">
           <p>Create an input form.</p>
         </div>
       </div>
       <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'End Of Flow')} draggable>
         <div>
-          <PauseIcon fontSize="medium" className="icon" /><span style={{ marginLeft: '18%' }}>End Of Flow</span>
+          <PauseIcon fontSize="medium" className="icon" /><span>End Of Flow</span>
         </div>
-        <div className='ml-6'>
+        <div className="sub-txt">
           <p>End the current flow.</p>
         </div>
       </div>
